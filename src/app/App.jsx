d5fab06addf5348369be2c7bf2657f87a6759835
khalil <olamilekanjinadu21@gmail.com>
2023-04-26 00:39:16 -0700

    trying to apply the Git REST Api

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,15 +6,20 @@ import api from "../api/api";
 
 function App() {
   const [info, setInfo] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   const createApiCall = async() => {
+    setIsLoading(true)
     try{
-      const response = await api.get('/')
+      const response = await api.get('/info')
       console.log(response.data)
       setInfo(response.data);
     }
     catch(err){
       console.error(err)
+    }
+    finally {
+      setIsLoading(false)
     }
   }
 
@@ -22,26 +27,26 @@ function App() {
     createApiCall();
   }, [])
 
-  const APIS = info.map((data) => {
-    return(
-      <tr>
-        <td>{data.id}</td>
-        <td>{data.title}</td>
-        <td>{data.body}</td>
-      </tr>
-    )
-  })
+
   return (
     <>
       <Router />
-      <table>
-      <tr>
-        <td>IDS</td>
-        <td>TITLES</td>
-        <td>BODY</td>
-      </tr>
-      {APIS}
-    </table>
+      {isLoading && <h5>Loading Repos</h5> }
+      {
+        info.length > 0 
+        ?
+        info.map(() => {
+          return(
+            <div key={id}>
+              <h4>{}</h4>
+              <p>{}</p>
+              <p>{}</p>
+            </div>
+          )
+        })
+        :
+        <h5>No data Avaialable</h5>
+      }
     </>
   )
 }
